f3f2ccfe4bd2a949985b5d2aeedc5effe627b3e2
alejandromya <alexxx.maya@gmail.com>
2025-06-24 19:12:41 +0200

    refactor

--- src/core/infrasestructura/searchMovieFromJson.ts
+++ src/core/infrasestructura/searchMovieFromJson.ts
@@ -3,7 +3,6 @@ import { MovieRepository } from "../dominio/MovieRepository";
 
 export class MovieFromJsonRepository implements MovieRepository {
   searchMovieByTitle(searchingText: string, movie: MovieType[]) {
-    if (searchingText.trim() === "") return [];
     const filteredFilms = movie.filter((movies) =>
       movies.title.toLowerCase().includes(searchingText.toLowerCase())
     );
